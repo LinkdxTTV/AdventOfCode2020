e0ce44bf7fc1ac6c5a3e906d922cdb0e63d001c9
linkdx <>
2020-12-09 23:50:35 -0800

    prob10

---
+++ prob10/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("~(OwO)~")
+	data, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	processedData := strings.Split(string(data), "\n")
+	joltDifferences := map[int]int{}
+	intMap := map[int]bool{}
+	intArray := []int{}
+	for _, num := range processedData {
+		numInt, err := strconv.Atoi(string(num))
+		fmt.Println(numInt)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		intMap[numInt] = true
+		intArray = append(intArray, numInt)
+	}
+
+	_, max := findMinAndMax(intArray)
+	intMap[max+3] = true
+	intArray = append(intArray, max+3)
+	currentJolt := 0
+	for i := 0; i <= max+3; i++ {
+		_, ok := intMap[i]
+		if !ok {
+			fmt.Println("didnt see ", i)
+			continue
+		}
+		diff := i - currentJolt
+		currentJolt += diff
+		if diff > 3 {
+			fmt.Println("wut", i, diff, currentJolt)
+			return
+		}
+		joltDifferences[diff]++
+	}
+
+	fmt.Println(joltDifferences)
+	fmt.Println(joltDifferences[1] * joltDifferences[3])
+
+	memo := map[int]int{}
+	recursiveFindWithMemo(0, max+3, intMap, memo)
+	fmt.Println("valid", memo[0])
+
+}
+
+// Stole this from the internet because Go for some reason doesnt have this functionality natively thank you google
+func findMinAndMax(a []int) (min int, max int) {
+	min = a[0]
+	max = a[0]
+	for _, value := range a {
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+	}
+	return min, max
+}
+
+func recursiveFindWithMemo(start, target int, intMap map[int]bool, memo map[int]int) int {
+	if start == target {
+		return 1
+	}
+	value, ok := memo[start]
+	if ok {
+		return value
+	}
+	children := 0
+	for i := start + 1; i <= start+3; i++ {
+		_, ok := intMap[i]
+		if ok {
+			children += recursiveFindWithMemo(i, target, intMap, valid, memo)
+		}
+	}
+	memo[start] = children
+	return children
+}
